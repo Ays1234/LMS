2558cb25798e1adbbab705e9189a558980b1ce12
Ays1234 <yogimaulana100@gmail.com>
2023-02-24 00:35:40 +0700

    belajar button click gitu

--- src/app/hello-word.component.ts
+++ src/app/hello-word.component.ts
@@ -2,9 +2,19 @@ import {Component } from '@angular/core'
 
 @Component({
 selector: 'hallo-wohrod',
-template: '<h2>Helloh Word</h2>'
+// template: '<h2>Helloh Word</h2>',
+templateUrl: './hello-tes.component.html',
+styles: ['h2, p{font-family: sans-serif; margin:10%}']
 })
 
 export class HelloWorldComponent{
-    
+    judul = "Hello Angular. zz"
+    gambar= "assets/YOGI.jpg"
+    active = true
+    // hello(){
+    //     alert(this.judul)
+    // }
+    hello(name: string){
+        alert('hallo ' + name)
+    }
 }

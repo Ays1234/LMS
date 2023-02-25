26f8be1ca3049c751415c369e6958cf9b758a339
Ays1234 <yogimaulana100@gmail.com>
2023-02-25 13:23:53 +0700

    belajar tanggal 25

--- src/app/hello-word.component.ts
+++ src/app/hello-word.component.ts
@@ -4,17 +4,21 @@ import {Component } from '@angular/core'
 selector: 'hallo-wohrod',
 // template: '<h2>Helloh Word</h2>',
 templateUrl: './hello-tes.component.html',
-styles: ['h2, p{font-family: sans-serif; margin:10%}']
+styles: ['h2, p{font-family: sans-serif;}']
 })
 
 export class HelloWorldComponent{
     judul = "Hello Angular. zz"
     gambar= "assets/YOGI.jpg"
-    active = true
+    active = false
     // hello(){
     //     alert(this.judul)
     // }
     hello(name: string){
-        alert('hallo ' + name)
+        alert('Hallo ' + name)
+    }
+
+    onEditClick(){
+        this.active =!this.active
     }
 }
